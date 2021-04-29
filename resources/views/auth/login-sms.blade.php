9eb974ab4b2481eaef2152a3862167f698812c14
rmahdi555 <rezaie.mehdi555@gmail.com>
2021-04-29 23:04:40 +0430

    change them web view 2

--- resources/views/auth/login-sms.blade.php
+++ resources/views/auth/login-sms.blade.php
@@ -1,6 +1,63 @@
 @extends('web.master')
 @section('content')
+    <section class="padding-top-index">
+    </section>
+    <!-- Contact Section Begin -->
+    <section class="contact-section spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="contact-form">
+                        <h2>{{ __('web/public.login') }} </h2>
+                        <form method="POST" action="{{ route('login.sms') }}">
+                            @csrf
+                            <div class="group-in">
+                                <label for="name">Name</label>
+                                <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror"
+                                       name="phone" value="{{ old('phone') }}" required autocomplete="phone" autofocus>
+                            </div>
+                            <div class="group-in">
+                                <label for="email">Email</label>
+                                <input type="text" id="email">
+                            </div>
+                            <div class="group-in">
+                                <label for="message">Message</label>
+                                <textarea id="message"></textarea>
+                            </div>
+                            <button type="submit">Submit Now</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="contact-info">
+                        <h2>Contact Info</h2>
+                        <p>It is a long established fact that a reader will be distracted by the readable content of a
+                            page when looking at its layout. The point of using Lorem Ipsum is that it has a
+                            distribution of letters.</p>
+                        <div class="ci-address">
+                            <h5>New York Office</h5>
+                            <ul>
+                                <li>T8/480 Collins St, Melbourne VIC 3000, New York</li>
+                                <li>1-234-567-89011</li>
+                                <li>info.colorlib@gmail.com </li>
+                            </ul>
+                        </div>
+                        <div class="ci-address">
+                            <h5>Australia Office</h5>
+                            <ul>
+                                <li>Forrest Ray, 191-103 Integer Rd, Corona Australia</li>
+                                <li>1-234-567-89011</li>
+                                <li>info.colorlib@gmail.com </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Contact Section End -->
 
+    <section class="latest-section">
     <div id="container">
         <div class="container">
             <!-- Breadcrumb Start-->
@@ -83,7 +140,7 @@
 
 
 
-
+    </section>
 
             @endsection
 
