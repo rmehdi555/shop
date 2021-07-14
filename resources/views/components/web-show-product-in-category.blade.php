185919dc773f8c32a9fd9b37ba704d7c261dba7f
unknown <Administrator@CC-16.Reyhoon.local>
2021-07-14 18:33:57 +0430

    edit product view

--- resources/views/components/web-show-product-in-category.blade.php
+++ resources/views/components/web-show-product-in-category.blade.php
@@ -4,12 +4,12 @@
 <table class="table table-striped table-responsive-stack " id="tableOne{{$category->id}}">
     <thead class="thead-dark">
     <tr>
-        <th>{{__('web/public.code')}}</th>
-        <th>{{__('web/public.title')}}</th>
-        <th>{{__('web/public.size')}}</th>
-        <th>{{__('web/public.standard')}}</th>
-        <th>{{__('web/public.unit')}}</th>
-        <th>{{__('web/public.price')}}</th>
+        <th class="hidden-mobile-view th-td-8" >{{__('web/public.code')}}</th>
+        <th class="th-td-50">{{__('web/public.title')}}</th>
+        <th class="hidden-mobile-view th-td-8" >{{__('web/public.size')}}</th>
+        <th class="hidden-mobile-view th-td-8" >{{__('web/public.standard')}}</th>
+        <th class="hidden-mobile-view th-td-10" >{{__('web/public.unit')}}</th>
+        <th class="th-td-16">{{__('web/public.price')}}</th>
     </tr>
     </thead>
     <tbody>
@@ -17,15 +17,15 @@
 
 
         <tr>
-            <td>{{$product->id}}</td>
-            <td>{{\App\Providers\MyProvider::_text($product->title)}}</td>
-            <td>{{empty($product->size)?"_":$product->size}}</td>
-            <td>{{empty($product->standard)?"_":$product->standard}}</td>
-            <td>{{empty($product->unit)?"_":__('web/public.unit_'.$product->unit)}}</td>
-            <td>@if($product->price==0) <a href="tel:{{\App\Providers\MyProvider::_text($siteDetailsProvider["phone"]->value)}}" target="_blank" rel="nofollow"  class="call-for-price">{{__('web/public.call_for_price')}}</a>
+            <td class="hidden-mobile-view th-td-8" >{{$product->id}}</td>
+            <td class="th-td-50"><a href="{{ route('web.show.product',$product->id) }}" class="title-link-in-list">{{\App\Providers\MyProvider::_text($product->title)}} </a></td>
+            <td class="hidden-mobile-view th-td-8" >{{empty($product->size)?"_":$product->size}}</td>
+            <td class="hidden-mobile-view th-td-8" >{{empty($product->standard)?"_":$product->standard}}</td>
+            <td class="hidden-mobile-view th-td-10" >{{empty($product->unit)?"_":__('web/public.unit_'.$product->unit)}}</td>
+            <td class="th-td-16">@if($product->price==0) <a href="tel:{{\App\Providers\MyProvider::_text($siteDetailsProvider["phone"]->value)}}" target="_blank" rel="nofollow"  class="call-for-price">{{__('web/public.call_for_price')}}</a>
                 @elseif(session('Local_Currency')=="USD" AND $product->price_usd!=0){{$product->price_usd}}{{__('web/public.currency_name_'.session('Local_Currency'))}}
                 @elseif(session('Local_Currency')=="EUR" AND $product->price_euro!=0){{$product->price_euro}}{{__('web/public.currency_name_'.session('Local_Currency'))}}
-                   @else {{\App\Providers\MyProvider::exToLocalDiscount($product->price,$product->discount)}}{{__('web/public.currency_name_'.session('Local_Currency'))}}@endif</td>
+                @else {{\App\Providers\MyProvider::exToLocalDiscount($product->price,$product->discount)}}{{__('web/public.currency_name_'.session('Local_Currency'))}}@endif</td>
         </tr>
 
     @endforeach
