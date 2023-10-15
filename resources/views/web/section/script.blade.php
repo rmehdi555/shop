5390889038d8518e2dd1f07167d942cb67affb9b
mehdi555 <rezaie.mehdi555@gmail.com>
2023-10-15 12:07:30 +0330

    Update script.blade.php

--- resources/views/web/section/script.blade.php
+++ resources/views/web/section/script.blade.php
@@ -9,7 +9,7 @@
 <script type="text/javascript" src="{{asset('web/2021/js/main-v-1-2-1.js?v='.time())}}"></script>
 <!---start GOFTINO code--->
 <script type="text/javascript">
-    !function(){var i="7pIYLr",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
+  !function(){var i="sPeiCG",a=window,d=document;function g(){var g=d.createElement("script"),s="https://www.goftino.com/widget/"+i,l=localStorage.getItem("goftino_"+i);g.async=!0,g.src=l?s+"?o="+l:s;d.getElementsByTagName("head")[0].appendChild(g);}"complete"===d.readyState?g():a.attachEvent?a.attachEvent("onload",g):a.addEventListener("load",g,!1);}();
 </script>
 <!---end GOFTINO code--->
 </body>
