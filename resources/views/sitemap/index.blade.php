1172549c5d148a1f2bc8cd35e11d88953e8cf5fe
rmahdi555 <rezaie.mehdi555@gmail.com>
2023-12-11 22:41:10 +0330

    etit sitemap

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,43 +1,39 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <sitemap>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
         <loc>{{ route('web.home') }}</loc>
         <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>{{ route('web.HomeController.show.all.milegerd') }}</loc>
+    </url>
+    <url>
+        <loc>{{ route('web.show.about_us') }}</loc>
         <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    <sitemap>
-        <loc>https://assen.ir/show/page/1</loc>
-        <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-    @if($products != null)
-        @foreach($products as $product)
-            <sitemap>
-                <loc>{{ route('web.show.product',$product->slug) }}</loc>
-                <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            </sitemap>
-        @endforeach
-    @endif
-    @if($news != null)
-        @foreach($news as $item)
-            <sitemap>
-                <loc>{{ route('web.show.news',$item->slug) }}</loc>
-                <lastmod>{{ $item->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            </sitemap>
-        @endforeach
-    @endif
+    </url>
     @if($productCategories != null)
         @foreach($productCategories as $item)
-            <sitemap>
+            <url>
                 <loc>{{ route('web.show.category',$item->slug) }}</loc>
                 <lastmod>{{ $item->updated_at->tz('UTC')->toAtomString() }}</lastmod>
-            </sitemap>
+            </url>
         @endforeach
     @endif
-    <sitemap>
+    @if($products != null)
+        @foreach($products as $product)
+            <url>
+                <loc>{{ route('web.show.product',$product->slug) }}</loc>
+                <lastmod>{{ $product->updated_at->tz('UTC')->toAtomString() }}</lastmod>
+            </url>
+        @endforeach
+    @endif
+    {{--@if($news != null)--}}
+    {{--@foreach($news as $item)--}}
+    {{--<url>--}}
+    {{--<loc>{{ route('web.show.news',$item->slug) }}</loc>--}}
+    {{--<lastmod>{{ $item->updated_at->tz('UTC')->toAtomString() }}</lastmod>--}}
+    {{--</url>--}}
+    {{--@endforeach--}}
+    {{--@endif--}}
+    <url>
         <loc>{{ route('web.contact.us.index') }}</loc>
         <lastmod>{{ now()->tz('UTC')->toAtomString() }}</lastmod>
-    </sitemap>
-</sitemapindex>
+    </url>
+</urlset>
